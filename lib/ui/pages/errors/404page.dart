7d4346c9d1a4b9bf14e7aff537bdcb4789bc3f66
ebabakg <babakgahremanzadeh@gmail.com>
2022-02-05 16:47:08 +0330

    error 404 page

--- lib/ui/pages/errors/404page.dart
+++ lib/ui/pages/errors/404page.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 class Page404 extends StatelessWidget {
   const Page404({Key? key}) : super(key: key);
 
-  static const routeName = '/404';
+  static const routeName = '/error404';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Text("404"),
       ),
