89b4bd8c4089b13c338a1cbcb2e9297a1c845c55
ebabakg <babakgahremanzadeh@gmail.com>
2022-03-18 11:16:07 +0330

    add base url to dio

--- lib/controller/providers/home_provider.dart
+++ lib/controller/providers/home_provider.dart
@@ -6,14 +6,17 @@ import 'package:flutter_course_phlox/model/model_headline.dart';
 class HomeProvider extends ChangeNotifier{
 
   bool loading = true;
-  final Dio _dio = Dio();
+  final Dio _dio = Dio(BaseOptions(
+    baseUrl: "https://api.phloxcompany.com/flutter_course/"
+  ));
   final List<ModelHeadline> _listHeadline = [];
 
   List<ModelHeadline> get listHeadlines => _listHeadline;
 
   Future<void> requestHeadline() async {
     Response response =
-    await _dio.get("https://api.phloxcompany.com/flutter_course/index.php");
+    await _dio.get("?api=headline");
+    debugPrint(response.data);
     if (response.statusCode == 200) {
       var _data = json.decode(response.data);
       var _responseListHeadline = _data['list'];
